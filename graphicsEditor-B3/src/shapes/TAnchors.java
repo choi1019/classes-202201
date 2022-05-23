bcebdd20743831d4c89dae5310fe2591f674a5cc
User <User@Choi-LG-Laptop>
2022-05-23 20:27:58 +0900

    2022-05-23

---
+++ graphicsEditor-B3/src/shapes/TAnchors.java
@@ -0,0 +1,141 @@
+package shapes;
+
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Point2D;
+
+public class TAnchors {
+	
+	private final static int WIDTH = 15;
+	private final static int HEIGHT = 15;
+	
+	public enum EAnchors {
+		eNW,
+		eWW,
+		eSW,
+		eSS,
+		eSE,
+		eEE,
+		eNE,
+		eNN,
+		eRR, 
+		eMove
+	}
+	private Ellipse2D anchors[];
+	private EAnchors eSelectedAnchor, eResizeAnchor;
+	
+	
+	public EAnchors geteSelectedAnchor() {
+		return eSelectedAnchor;
+	}
+	public void seteSelectedAnchor(EAnchors eSelectedAnchor) {
+		this.eSelectedAnchor = eSelectedAnchor;
+	}
+
+	
+	public TAnchors() {
+		this.anchors = new Ellipse2D[EAnchors.values().length-1];
+		for (int i=0; i<EAnchors.values().length-1; i++) {
+			this.anchors[i] = new Ellipse2D.Double();
+		}
+	}
+	
+	private void setAnchors(Rectangle BoundingRectangle, AffineTransform affineTransform) {		
+		for (int i=0; i<EAnchors.values().length-1; i++) {			
+			double x = BoundingRectangle.x - WIDTH/2.0;
+			double y = BoundingRectangle.y - HEIGHT/2.0;
+			double w = BoundingRectangle.width;
+			double h = BoundingRectangle.height;
+			
+			EAnchors eAnchor = EAnchors.values()[i];
+			switch (eAnchor) {
+				case eNW: 								break;
+				case eWW: 				y = y + h/2; 	break;
+				case eSW:				y = y + h; 		break;
+				case eSS: x = x + w/2;	y = y + h;		break;
+				case eSE: x = x + w;	y = y + h;		break;
+				case eEE: x = x + w;	y = y + h/2;	break;
+				case eNE: x = x + w;					break;
+				case eNN: x = x + w/2;					break;
+				case eRR: x = x + w/2;	y = y - h/2;	break;
+				default:								break;
+			}
+			Point2D point = new Point2D.Double(x, y);
+			affineTransform.transform(point, point);
+			this.anchors[i].setFrame(point.getX(), point.getY(), WIDTH, HEIGHT);
+		}
+	}
+	
+	public boolean contains(int x, int y, Rectangle BoundingRectangle, AffineTransform affineTransform) {
+		this.setAnchors(BoundingRectangle, affineTransform);
+		for (int i=0; i<EAnchors.values().length-1; i++) {
+			if (this.anchors[i].contains(x, y)) {
+				this.eSelectedAnchor = EAnchors.values()[i];
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public void draw(Graphics2D graphics2D, Rectangle BoundingRectangle, AffineTransform affineTransform) {
+		this.setAnchors(BoundingRectangle, affineTransform);
+		for (int i=0; i<EAnchors.values().length-1; i++) {
+			graphics2D.draw(this.anchors[i]);
+		}
+	}
+	
+	public Point2D getResizeAnchorPosition() {
+		return new Point2D.Double(
+				this.anchors[this.eResizeAnchor.ordinal()].getCenterX(),
+				this.anchors[this.eResizeAnchor.ordinal()].getCenterY()
+		);
+	}
+	
+	public void setResizeAnchor() {
+		switch (this.eSelectedAnchor) {
+			case eNW: this.eResizeAnchor = EAnchors.eSE;	break;
+			case eWW: this.eResizeAnchor = EAnchors.eEE;	break;
+			case eSW: this.eResizeAnchor = EAnchors.eNE;	break;
+			case eSS: this.eResizeAnchor = EAnchors.eNN;	break;
+			case eSE: this.eResizeAnchor = EAnchors.eNW; 	break;
+			case eEE: this.eResizeAnchor = EAnchors.eWW;	break;
+			case eNE: this.eResizeAnchor = EAnchors.eSW;	break;
+			case eNN: this.eResizeAnchor = EAnchors.eSS;	break;
+			default: break;
+		}
+	}
+	
+	public Point2D getResizeScale(int x, int y, int px, int py, Shape shape, AffineTransform affineTransform) {
+		Shape transformedShape = affineTransform.createTransformedShape(shape);
+		double width = transformedShape.getBounds().width;
+		double height = transformedShape.getBounds().height;
+		
+		double dw = x-px;
+		double dh = y-py;
+		switch (this.eResizeAnchor) {
+			case eEE: dw = -dw;		dh=  0;		break;
+			case eWW:				dh=  0;		break;
+			case eSS: dw =  0;		dh=  -dh;	break;
+			case eNN: dw =  0;					break;
+	
+			case eSE: dw =  -dw;	dh= -dh;	break;
+			case eNE: dw =  -dw;				break;
+			case eSW: 				dh= -dh;	break;	
+			case eNW: 							break;
+			default: break;
+		}
+		// compute resize 
+		double xf = 1.0;
+		double yf = 1.0;
+		if (width > 0.0)
+			xf = dw / width + xf;
+		if (height > 0.0)			
+			yf = dh / height + yf;
+		
+		return new Point2D.Double(xf, yf);
+	}
+
+}
